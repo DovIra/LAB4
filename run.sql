4a9dcee06905f42b860e80454c0c54e2bf82875c
DovIra <44786442+DovIra@users.noreply.github.com>
2020-05-24 23:41:13 +0300

    Update run.sql

--- run.sql
+++ run.sql
@@ -3,8 +3,8 @@ declare
     emploee_name_check varchar(10);
     location_name varchar(50);
     location_name_check varchar(50);
-    
-    cursor curr1 is 
+
+    cursor curr1 is
     select
         to_char(WEATHER_DATE, 'dd.mm.yyyy') as weather_date,
         LOCATION_CODE,
@@ -12,12 +12,17 @@ declare
         is_raining(WEATHER_DATE, LOCATION_CODE, 0) as rainTomorrow
     from WEATHER_DAILY
     order by LOCATION_CODE, weather_date;
-BEGIN 
+
+    cursor curr2 is
+    select t.time as time, t.temperature as temperature
+    from table(get_hourly_data('Canberra', to_date('2017-01-07', 'yyyy-mm-dd'))) t
+    order by time;
+BEGIN
     emploee_name := 'Bob';
     location_name := 'Darwin';
-    
+
     DBMS_OUTPUT.enable;
-    
+
     DBMS_OUTPUT.put_line('Example with exception due to absent of employee in the staff table:');
     ADD_LOCATION_CHIEF(emploee_name || '_Absent', location_name);
     DBMS_OUTPUT.put_line('     ~~~');
@@ -28,13 +33,22 @@ BEGIN
     DBMS_OUTPUT.put_line('Employee: ' || emploee_name);
     DBMS_OUTPUT.put_line('Location: ' || location_name);
     ADD_LOCATION_CHIEF(emploee_name, location_name);
-    
+
     select location, LOCATION_CHIEF
     into location_name_check, emploee_name_check
     from WEATHER_DETAILS
     where location = location_name and LOCATION_CHIEF = emploee_name;
-    
+
     DBMS_OUTPUT.put_line('Result: employee ' || emploee_name_check || ' is chief of the ' || location_name_check || ' location.');
+    DBMS_OUTPUT.put_line('--------------');
+    DBMS_OUTPUT.put_line('');
+
+    DBMS_OUTPUT.put_line('Example for demonstrate work of pipelined function get_hourly_data().');
+    DBMS_OUTPUT.put_line('temperature dynamics for 07/01/2017 for Canberra location:');
+    FOR i IN curr2
+    LOOP
+        DBMS_OUTPUT.put_line('        at: ' || i.time || ' hour temperature is ' || i.temperature);
+    END LOOP;
     DBMS_OUTPUT.put_line('--------------');
     DBMS_OUTPUT.put_line('');
     DBMS_OUTPUT.put_line('Example for demonstrate work of function IS_RAINING():');
@@ -42,5 +56,5 @@ BEGIN
     LOOP
         DBMS_OUTPUT.put_line('Location: ' || weather_iteration.LOCATION_CODE || ', date: ' || weather_iteration.weather_date || ' rain today: ' || weather_iteration.rainToday || ', rain tomorrow: ' || weather_iteration.rainTomorrow);
     END LOOP;
-    
+
 END;
