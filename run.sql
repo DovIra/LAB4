3860046b5af570d8883dcefbeedde0e74fec2600
DovIra <44786442+DovIra@users.noreply.github.com>
2020-05-24 23:29:04 +0300

    Add files via upload

---
+++ run.sql
@@ -0,0 +1,46 @@
+declare
+    emploee_name varchar(10);
+    emploee_name_check varchar(10);
+    location_name varchar(50);
+    location_name_check varchar(50);
+    
+    cursor curr1 is 
+    select
+        to_char(WEATHER_DATE, 'dd.mm.yyyy') as weather_date,
+        LOCATION_CODE,
+        is_raining(WEATHER_DATE, LOCATION_CODE) as rainToday,
+        is_raining(WEATHER_DATE, LOCATION_CODE, 0) as rainTomorrow
+    from WEATHER_DAILY
+    order by LOCATION_CODE, weather_date;
+BEGIN 
+    emploee_name := 'Bob';
+    location_name := 'Darwin';
+    
+    DBMS_OUTPUT.enable;
+    
+    DBMS_OUTPUT.put_line('Example with exception due to absent of employee in the staff table:');
+    ADD_LOCATION_CHIEF(emploee_name || '_Absent', location_name);
+    DBMS_OUTPUT.put_line('     ~~~');
+    DBMS_OUTPUT.put_line('Example with exception due to absent of location in the corresponding table:');
+    ADD_LOCATION_CHIEF(emploee_name, location_name || '_Absent');
+    DBMS_OUTPUT.put_line('     ~~~');
+    DBMS_OUTPUT.put_line('Adding exists employee as chief to the exists location:');
+    DBMS_OUTPUT.put_line('Employee: ' || emploee_name);
+    DBMS_OUTPUT.put_line('Location: ' || location_name);
+    ADD_LOCATION_CHIEF(emploee_name, location_name);
+    
+    select location, LOCATION_CHIEF
+    into location_name_check, emploee_name_check
+    from WEATHER_DETAILS
+    where location = location_name and LOCATION_CHIEF = emploee_name;
+    
+    DBMS_OUTPUT.put_line('Result: employee ' || emploee_name_check || ' is chief of the ' || location_name_check || ' location.');
+    DBMS_OUTPUT.put_line('--------------');
+    DBMS_OUTPUT.put_line('');
+    DBMS_OUTPUT.put_line('Example for demonstrate work of function IS_RAINING():');
+    FOR weather_iteration IN curr1
+    LOOP
+        DBMS_OUTPUT.put_line('Location: ' || weather_iteration.LOCATION_CODE || ', date: ' || weather_iteration.weather_date || ' rain today: ' || weather_iteration.rainToday || ', rain tomorrow: ' || weather_iteration.rainTomorrow);
+    END LOOP;
+    
+END;
